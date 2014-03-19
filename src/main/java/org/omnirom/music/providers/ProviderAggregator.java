7372f81ae1b7e05cb4518888ec11b21b18be22d2
xplodwild <me@xplod.fr>
2014-03-19 22:24:18 +0100

    Initial import of initial file tree

---
+++ src/main/java/org/omnirom/music/providers/ProviderAggregator.java
@@ -0,0 +1,38 @@
+package org.omnirom.music.providers;
+
+import org.omnirom.music.providers.IProviderUpdateCallback;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ProviderAggregator {
+
+    private List<IProviderUpdateCallback> mUpdateCallbacks;
+
+    private final static ProviderAggregator INSTANCE = new ProviderAggregator();
+
+    public final static ProviderAggregator getDefault() {
+        return INSTANCE;
+    }
+
+    private ProviderAggregator() {
+        mUpdateCallbacks = new ArrayList<IProviderUpdateCallback>();
+    }
+
+    public void addUpdateCallback(IProviderUpdateCallback cb) {
+        mUpdateCallbacks.add(cb);
+    }
+
+    public void removeUpdateCallback(IProviderUpdateCallback cb) {
+        mUpdateCallbacks.remove(cb);
+    }
+
+    public Iterator<IProviderUpdateCallback> getUpdateCallbacks() {
+        return mUpdateCallbacks.iterator();
+    }
+
+    public void search(final String query, final ISearchCallback callback) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+}
