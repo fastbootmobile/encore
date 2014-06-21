29a3d890a7e6c1afefbe19f0521f00aa5f0ddb96
xplodwild <me@xplod.fr>
2014-06-21 19:57:27 +0200

    PlayingBar: Fix play button click zone

--- src/main/java/org/omnirom/music/app/ui/PlayingBarView.java
+++ src/main/java/org/omnirom/music/app/ui/PlayingBarView.java
@@ -9,6 +9,7 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
@@ -148,7 +149,8 @@ public class PlayingBarView extends RelativeLayout {
     private Song mSong;
     private boolean mIsPlaying;
     private ProgressBar mScrobble;
-    private ImageView mPlayPauseButton;
+    private FrameLayout mPlayPauseButton;
+    private ImageView mPlayPauseInner;
     private ImageView mAlbumArt;
     private TextView mArtistView;
     private TextView mTitleView;
@@ -193,7 +195,8 @@ public class PlayingBarView extends RelativeLayout {
         });
 
         mScrobble           = (ProgressBar) findViewById(R.id.pbScrobble);
-        mPlayPauseButton    = (ImageView)   findViewById(R.id.btnPlay);
+        mPlayPauseButton    = (FrameLayout)   findViewById(R.id.btnPlay);
+        mPlayPauseInner     = (ImageView)   findViewById(R.id.btnPlayInner);
         mAlbumArt           = (ImageView)   findViewById(R.id.ivAlbumArt);
         mArtistView         = (TextView)    findViewById(R.id.tvArtist);
         mTitleView          = (TextView)    findViewById(R.id.tvTitle);
@@ -236,9 +239,9 @@ public class PlayingBarView extends RelativeLayout {
      */
     public void setPlayButtonState(boolean play) {
         if (play) {
-            mPlayPauseButton.setImageResource(R.drawable.ic_btn_play);
+            mPlayPauseInner.setImageResource(R.drawable.ic_btn_play);
         } else {
-            mPlayPauseButton.setImageResource(R.drawable.ic_btn_pause);
+            mPlayPauseInner.setImageResource(R.drawable.ic_btn_pause);
         }
     }
 
