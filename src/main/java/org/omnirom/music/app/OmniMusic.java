2278b786188ae3c9df4a698c366d6472dcfbc4a4
xplodwild <xplodgui@gmail.com>
2014-09-12 10:24:17 +0200

    Add ACRA bug reporting

--- src/main/java/org/omnirom/music/app/OmniMusic.java
+++ src/main/java/org/omnirom/music/app/OmniMusic.java
@@ -4,6 +4,10 @@ import android.app.Application;
 import android.net.http.HttpResponseCache;
 import android.util.Log;
 
+import org.acra.ACRA;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
+import org.acra.sender.HttpSender;
 import org.omnirom.music.api.echonest.AutoMixManager;
 import org.omnirom.music.framework.AlbumArtCache;
 import org.omnirom.music.framework.ImageCache;
@@ -13,12 +17,31 @@ import org.omnirom.music.providers.ProviderAggregator;
 import java.io.File;
 import java.io.IOException;
 
+@ReportsCrashes(
+        formKey = "",
+        httpMethod = HttpSender.Method.PUT,
+        reportType = HttpSender.Type.JSON,
+        formUri = "http://nuclear.ouverta.fr/om-acra/_design/acra-storage/_update/report",
+        formUriBasicAuthLogin = "reporter-omnimusic-alpha",
+        formUriBasicAuthPassword = "4lqh4mu51c1337",
+
+        // Dialog during DEBUG, TOAST on prod
+        mode = ReportingInteractionMode.DIALOG,
+        resToastText = R.string.crash_toast_text, // optional, displayed as soon as the crash occurs, before collecting data which can take a few seconds
+        resDialogText = R.string.crash_dialog_text,
+        resDialogIcon = android.R.drawable.ic_dialog_info, //optional. default is a warning sign
+        resDialogTitle = R.string.crash_dialog_title, // optional. default is your application name
+        resDialogCommentPrompt = R.string.crash_dialog_comment_prompt, // optional. when defined, adds a user text field input with this text resource as a label
+        resDialogOkToast = R.string.crash_dialog_ok_toast // optional. displays a Toast message when the user accepts to send a report.
+)
 public class OmniMusic extends Application {
     private static final String TAG = "OmniMusic";
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        ACRA.init(this);
 
         // Setup the plugins system
         ProviderAggregator.getDefault().setContext(getApplicationContext());
@@ -47,5 +70,6 @@ public class OmniMusic extends Application {
         // Setup Automix system
         AutoMixManager.getDefault().initialize(getApplicationContext());
     }
+
 
 }
