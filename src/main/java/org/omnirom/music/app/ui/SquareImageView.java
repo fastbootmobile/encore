5a665ce15ad841d95b9c077fccc327feadea4143
xplodwild <me@xplod.fr>
2014-10-23 16:32:30 +0200

    SquareImageView: Add header and comments

--- src/main/java/org/omnirom/music/app/ui/SquareImageView.java
+++ src/main/java/org/omnirom/music/app/ui/SquareImageView.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2014 Fastboot Mobile, LLC.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+ * the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, see <http://www.gnu.org/licenses>.
+ */
+
 package org.omnirom.music.app.ui;
 
 import android.content.Context;
@@ -7,7 +22,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 /**
- * Created by xplodwild on 7/7/14.
+ * ImageView that remains a square aspect ratio (if no exact size has been set)
  */
 public class SquareImageView extends ImageView {
 
