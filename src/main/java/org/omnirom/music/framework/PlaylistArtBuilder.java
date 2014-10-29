d444f7fc12627852940746faf2dfeb97b6899cc2
xplodwild <me@xplod.fr>
2014-10-29 18:58:27 +0100

    Some code lint

--- src/main/java/org/omnirom/music/framework/PlaylistArtBuilder.java
+++ src/main/java/org/omnirom/music/framework/PlaylistArtBuilder.java
@@ -1,6 +1,20 @@
+/*
+ * Copyright (C) 2014 Fastboot Mobile, LLC.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+ * the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, see <http://www.gnu.org/licenses>.
+ */
+
 package org.omnirom.music.framework;
 
-import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
@@ -20,7 +34,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by Guigui on 29/10/2014.
+ * Class creating a composite image for playlist cover art
  */
 public class PlaylistArtBuilder {
     private Bitmap mPlaylistComposite;
