e69740645f938ca2cfcdfba49f4a39bef8144b4b
XpLoDWilD <me@xplod.fr>
2016-09-08 22:40:36 +0200

    Request EXTERNAL_STORAGE permission for music library

--- src/main/java/com/fastbootmobile/encore/app/MainActivity.java
+++ src/main/java/com/fastbootmobile/encore/app/MainActivity.java
@@ -15,6 +15,7 @@
 
 package com.fastbootmobile.encore.app;
 
+import android.*;
 import android.app.AlertDialog;
 import android.app.SearchManager;
 import android.app.TimePickerDialog;
@@ -22,6 +23,7 @@ import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.graphics.drawable.ColorDrawable;
 import android.net.http.HttpResponseCache;
@@ -29,9 +31,11 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.RemoteException;
 import android.os.SystemClock;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.MediaRouteActionProvider;
@@ -83,6 +87,8 @@ public class MainActivity extends AppActivity
     public static final int SECTION_DRIVE_MODE = 9;
     public static final int SECTION_SETTINGS = 10;
 
+    private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 129;
+
     /**
      * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
      */
@@ -127,6 +133,14 @@ public class MainActivity extends AppActivity
             // Load UI
             setContentView(R.layout.activity_main);
 
+            // Ensure we have READ_EXTERNAL_STORAGE for Music database in LocalProvider
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+            }
+
             mToolbar = (Toolbar) findViewById(R.id.toolbar);
             setSupportActionBar(mToolbar);
 
@@ -161,6 +175,25 @@ public class MainActivity extends AppActivity
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (!(grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // permission denied, boo!
+                    Toast.makeText(this, R.string.toast_storage_permission_warning, Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 
     public Toolbar getToolbar() {
